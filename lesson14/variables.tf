58518518a4fc89f67c7312f619cf109f024a66e5
Aleksandr Romanov <rzn1nsky@yandex.ru>
2024-08-23 01:19:47 +0300

    Add terraform conf

---
+++ lesson14/variables.tf
@@ -0,0 +1,29 @@
+variable "yandex_cloud_id" {
+  type    = string
+  default = ""
+}
+
+variable "yandex_folder_id" {
+  type    = string
+  default = ""
+}
+
+variable "default_zone" {
+  type    = string
+  default = ""
+}
+
+variable "key_file" {
+  type    = string
+  default = ""
+}
+
+variable "ssh_public_key_path" {
+  type    = string
+  default = ""
+}
+
+variable "ssh_private_key_path" {
+  type    = string
+  default = ""
+}
