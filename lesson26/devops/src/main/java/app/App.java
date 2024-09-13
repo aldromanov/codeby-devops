aa3eb8e0c92ff0720f3a037d1c59a9521395488c
Aleksandr Romanov <rzn1nsky@yandex.ru>
2024-09-13 09:35:48 +0300

    Add files

---
+++ lesson26/devops/src/main/java/app/App.java
@@ -0,0 +1,20 @@
+package app;
+
+/**
+ * Hello Devops!
+ */
+
+public class App {
+
+    private static final String MESSAGE = "Hello Devops!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
